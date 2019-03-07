7a67193d9e5f4109ab67c57129fc39d38146f7fa
ghosoun89 <goson.aldabea98@gmail.com>
2019-03-07 12:19:35 +0200

    deploy

--- server/server.js
+++ server/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const bodyparser = require('body-parser');
+const path = require('path')
 // we import cors to connect front end with back end be cause each one 
 // in different host ,so we use cors to run 
 // both of them in the same host
 const cors = require('cors');
+app.use(express.static('../dist/server'))
+app.get('/*', (req,res) => {
+    res.sendFile(path.join(__dirname, '/dist/server/index.html'));
+});
 
 const PORT = 3000
 const api = require('./routes/api')
